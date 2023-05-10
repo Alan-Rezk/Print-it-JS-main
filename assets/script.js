0719c550b8eda143b57aae6dce5105a696d529db
Alan Rezk <Pro.alanrezk@gmail.com>
2023-05-10 11:10:10 +0200

    suppression du dossier slideshow, mise à la racine des slides.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const slides = [
 	{
-	  "image": "slideshow/slide1.jpg",
+	  "image": "slide1.jpg",
 	  "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-	  "image": "slideshow/slide2.jpg",
+	  "image": "slide2.jpg",
 	  "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-	  "image": "slideshow/slide3.jpg",
+	  "image": "slide3.jpg",
 	  "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-	  "image": "slideshow/slide4.png",
+	  "image": "slide4.png",
 	  "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
   ];
